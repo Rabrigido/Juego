064dd0d7ff5bd4236be05c555b67973254323109
nicopina <64911016+nicopina@users.noreply.github.com>
2020-05-30 00:58:27 -0400

    Alpha 0.1

--- Assets/Players/Player1controller.cs
+++ Assets/Players/Player1controller.cs
@@ -1,9 +1,11 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player1controller : MonoBehaviour
 {
+    Boolean canjump;
     // Start is called before the first frame update
     void Start()
     {
@@ -12,24 +14,40 @@ public class Player1controller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey("left"))
+        if (Input.GetKey("a"))
         {
             gameObject.transform.Translate(-10f * Time.deltaTime, 0, 0);
             gameObject.GetComponent<Animator>().SetBool("Move", true);
             gameObject.GetComponent<SpriteRenderer>().flipX = true;
         }
 
-        if (Input.GetKey("right"))
+        if (Input.GetKey("d"))
         {
             gameObject.transform.Translate(10f * Time.deltaTime, 0, 0);
             gameObject.GetComponent<Animator>().SetBool("Move", true);
             gameObject.GetComponent<SpriteRenderer>().flipX = false;
         }
-        if (!Input.GetKey("right") && !Input.GetKey("left"))
+        if (!Input.GetKey("d") && !Input.GetKey("a"))
         {
             gameObject.GetComponent<Animator>().SetBool("Move", false);
             gameObject.GetComponent<Animator>().SetBool("Move", false);
 
+        }
+        if (Input.GetKey("d") && Input.GetKey("c"))
+        {
+            gameObject.transform.Translate(10f * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("a") && Input.GetKey("c"))
+        {
+            gameObject.transform.Translate(-10f * Time.deltaTime, 0, 0);
+        }
+    }
+
+    private void OnCollosionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.tag == "Ground")
+        {
+            canjump = true;
         }
     }
 }
