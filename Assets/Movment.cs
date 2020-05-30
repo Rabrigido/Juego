0bede468d4d53519bc76541888da7f0575e29e5a
nicopina <64911016+nicopina@users.noreply.github.com>
2020-05-29 23:11:54 -0400

    1

---
+++ Assets/Movment.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class Movment : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("a"))
+        {
+            gameObject.transform.Translate(-10f * Time.deltaTime, 0, 0);
+            gameObject.GetComponent<Animator>().SetBool("Move", true);
+			gameObject.GetComponent<SpriteRenderer>().flipX = true;
+        }
+		
+        if (Input.GetKey("d"))
+        {
+            gameObject.transform.Translate(10f * Time.deltaTime, 0, 0);
+            gameObject.GetComponent<Animator>().SetBool("Move", true);
+			gameObject.GetComponent<SpriteRenderer>().flipX = false;
+        }
+        if (!Input.GetKey("d") && !Input.GetKey("a"))
+        {
+            gameObject.GetComponent<Animator>().SetBool("Move", false);
+            gameObject.GetComponent<Animator>().SetBool("Move", false);
+
+        }
+    }
+}
---
+++ Assets/PlayerControler.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("left"))
+        {
+            gameObject.transform.Translate(-10f * Time.deltaTime, 0, 0);
+            gameObject.GetComponent<Animator>().SetBool("Move", true);
+			gameObject.GetComponent<SpriteRenderer>().flipX = true;
+        }
+		
+        if (Input.GetKey("right"))
+        {
+            gameObject.transform.Translate(10f * Time.deltaTime, 0, 0);
+            gameObject.GetComponent<Animator>().SetBool("Move", true);
+			gameObject.GetComponent<SpriteRenderer>().flipX = false;
+        }
+        if (!Input.GetKey("right") && !Input.GetKey("left"))
+        {
+            gameObject.GetComponent<Animator>().SetBool("Move", false);
+            gameObject.GetComponent<Animator>().SetBool("Move", false);
+
+        }
+    }
+}
