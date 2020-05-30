d7bb9298d186ba577a35f577127079cda0891aa4
nicopina <64911016+nicopina@users.noreply.github.com>
2020-05-29 23:51:28 -0400

    fg

--- Assets/Players/Player1controller.cs
+++ Assets/Players/Player1controller.cs
@@ -1,9 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using System.Security.Cryptography;
 using UnityEngine;
 
-public class Movment : MonoBehaviour
+public class Player1controller : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -13,20 +12,20 @@ public class Movment : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey("a"))
+        if (Input.GetKey("left"))
         {
             gameObject.transform.Translate(-10f * Time.deltaTime, 0, 0);
             gameObject.GetComponent<Animator>().SetBool("Move", true);
-			gameObject.GetComponent<SpriteRenderer>().flipX = true;
+            gameObject.GetComponent<SpriteRenderer>().flipX = true;
         }
-		
-        if (Input.GetKey("d"))
+
+        if (Input.GetKey("right"))
         {
             gameObject.transform.Translate(10f * Time.deltaTime, 0, 0);
             gameObject.GetComponent<Animator>().SetBool("Move", true);
-			gameObject.GetComponent<SpriteRenderer>().flipX = false;
+            gameObject.GetComponent<SpriteRenderer>().flipX = false;
         }
-        if (!Input.GetKey("d") && !Input.GetKey("a"))
+        if (!Input.GetKey("right") && !Input.GetKey("left"))
         {
             gameObject.GetComponent<Animator>().SetBool("Move", false);
             gameObject.GetComponent<Animator>().SetBool("Move", false);
