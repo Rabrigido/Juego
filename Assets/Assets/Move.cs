f91d5e4ad0e8c2f2f97bb3e421b4e3dd6ce0e823
Cristhian <cristhian.rabi@alumnos.ucn.cl>
2020-05-29 23:05:56 -0400

    Assets antiguos

---
+++ Assets/Assets/Move.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }    
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey("left"))
+        {
+            gameObject.transform.Translate(-2f * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("right"))
+        {
+            gameObject.transform.Translate(2f * Time.deltaTime, 0, 0);
+        }        
+    }
+}
