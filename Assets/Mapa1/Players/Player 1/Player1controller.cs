c4d26aa221ee220543447d12723abd9b1b01d15b
nicopina <64911016+nicopina@users.noreply.github.com>
2020-05-31 20:37:34 -0400

    alpha 0.2

--- Assets/Mapa1/Players/Player 1/Player1controller.cs
+++ Assets/Mapa1/Players/Player 1/Player1controller.cs
@@ -7,12 +7,14 @@ public class Player1controller : MonoBehaviour
 {
     public float jumpForce;
     public bool onGround;
+    public Transform footRefLeft;
     public Transform footRef;
+    public Transform footRefRight;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
     // Update is called once per frame
     void Update()
@@ -30,16 +32,36 @@ public class Player1controller : MonoBehaviour
             gameObject.GetComponent<Animator>().SetBool("Move", true);
             gameObject.GetComponent<SpriteRenderer>().flipX = false;
         }
+
+        if (Input.GetKey("s"))
+        {
+            gameObject.GetComponent<Animator>().SetBool("Crouch", true);
+        }
+
+        if (!Input.GetKey("s"))
+        {
+            gameObject.GetComponent<Animator>().SetBool("Crouch", false);
+        }
+
         if (!Input.GetKey("d") && !Input.GetKey("a"))
         {
             gameObject.GetComponent<Animator>().SetBool("Move", false);
             gameObject.GetComponent<Animator>().SetBool("Move", false);
 
         }
+
+        if (Input.GetKey("d") && Input.GetKey("a"))
+        {
+            gameObject.GetComponent<Animator>().SetBool("Move", false);
+            gameObject.GetComponent<Animator>().SetBool("Move", false);
+
+        }
+
         if (Input.GetKey("d") && Input.GetKey("c"))
         {
             gameObject.transform.Translate(10f * Time.deltaTime, 0, 0);
         }
+
         if (Input.GetKey("a") && Input.GetKey("c"))
         {
             gameObject.transform.Translate(-10f * Time.deltaTime, 0, 0);
@@ -47,15 +69,17 @@ public class Player1controller : MonoBehaviour
 
         onGround = Physics2D.OverlapCircle(footRef.position, 0.5f, 1 << 8);
 
-        if (Input.GetKeyDown("w") && onGround)
+        if (Input.GetKeyDown("w") && onGround && !Input.GetKey("s"))
         {
             gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce));
 
         }
+
         if (!onGround)
         {
             gameObject.GetComponent<Animator>().SetBool("Jump", true);
         }
+
         if (onGround)
         {
             gameObject.GetComponent<Animator>().SetBool("Jump", false);
