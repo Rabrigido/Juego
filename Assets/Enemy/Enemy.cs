915b7531a03538c6a57ccc77f97d6801252a03c5
javieress <64340938+javieress@users.noreply.github.com>
2020-06-07 19:31:18 -0400

    Enemy 0.1

---
+++ Assets/Enemy/Enemy.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Diagnostics;
+using System.Media;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    // Variables para gestionar el radio de vision y velocidad
+    public float visionRadius;
+    public float speed;
+    public GameObject player;
+
+    //Variable para guardar la posicion
+    //GameObject player;
+    //Variable para guardar la posicion inicial
+    Vector3 initialPosition;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {   
+        //Recuperamos al jugador gracias al tag
+        //player = GameObject.FindGameObjectWithTag("Player");
+
+        //Guardamos nuestra posicion inicial
+        initialPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Por defecto nuestro objetivo siempre sera nuestra posicion inicial 
+        Vector3 target = initialPosition;
+        //Pero si la distancia hasta el jugador es menor que el radio de vision el objetivo sera él
+        float dist = Vector3.Distance(player.transform.position, transform.position);
+        if (dist < visionRadius) target = player.transform.position;
+
+        //Finalmente movemos el enemigo en direccion a su target
+        float fixedSpeed = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target, fixedSpeed);
+    }
+
+    void OnDrawGizmos()
+        {
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawWireSphere(transform.position, visionRadius);
+        }
+
+
+
+    
+}
