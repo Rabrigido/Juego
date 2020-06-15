4af4ef550aa6ecc6ce79ce3c8b46d65e88c506e7
nicopina <64911016+nicopina@users.noreply.github.com>
2020-06-14 20:08:07 -0400

    Tutorial 0.1
    Mejora en el código del player 1

--- Assets/Players/Player 1/Player1controller.cs
+++ Assets/Players/Player 1/Player1controller.cs
@@ -5,77 +5,84 @@ using UnityEngine;
 
 public class Player1controller : MonoBehaviour
 {
-    public float jumpForce;
+    public float jump_Force;
+    public float walk_Speed;
+    public float run_Speed;
     public bool onGround;
     public Transform footRefLeft;
     public Transform footRef;
     public Transform footRefRight;
+    public GameObject gun;
 
     // Start is called before the first frame update
     void Start()
     {
-            print(footRef.position.y.CompareTo(gameObject.transform.position.y));
+        
     }
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey("a"))
+        onGround = Physics2D.OverlapCircle(footRef.position, 0.5f, 1 << 8);
+
+        if (Input.GetKey("a") && (!Input.GetKey("s") || !onGround))
         {
-            gameObject.transform.Translate(-10f * Time.deltaTime, 0, 0);
+            gameObject.transform.Translate(-walk_Speed * Time.deltaTime, 0, 0);
             gameObject.GetComponent<Animator>().SetBool("Move", true);
             gameObject.GetComponent<SpriteRenderer>().flipX = true;
+            
         }
 
-        if (Input.GetKey("d"))
+        if (Input.GetKey("d") && (!Input.GetKey("s") || !onGround))
         {
-            gameObject.transform.Translate(10f * Time.deltaTime, 0, 0);
+            gameObject.transform.Translate(walk_Speed * Time.deltaTime, 0, 0);
             gameObject.GetComponent<Animator>().SetBool("Move", true);
             gameObject.GetComponent<SpriteRenderer>().flipX = false;
+
         }
 
-        if (Input.GetKey("s"))
+            if (Input.GetKey("s"))
         {
             gameObject.GetComponent<Animator>().SetBool("Crouch", true);
+            gameObject.GetComponent<Animator>().SetBool("Move", false);
+            gameObject.GetComponent<Animator>().SetBool("Jump", false);
         }
-
         if (!Input.GetKey("s"))
         {
             gameObject.GetComponent<Animator>().SetBool("Crouch", false);
         }
 
-        if (!Input.GetKey("d") && !Input.GetKey("a"))
+        if (Input.GetKey("s") && Input.GetKey("a"))
         {
-            gameObject.GetComponent<Animator>().SetBool("Move", false);
-            gameObject.GetComponent<Animator>().SetBool("Move", false);
-
+            gameObject.GetComponent<SpriteRenderer>().flipX = true;
         }
 
-        if (Input.GetKey("d") && Input.GetKey("a"))
+        if (Input.GetKey("s") && Input.GetKey("d"))
+        {
+            gameObject.GetComponent<SpriteRenderer>().flipX = false;
+        }
+
+        if ((!Input.GetKey("d") && !Input.GetKey("a")) || (Input.GetKey("d") && Input.GetKey("a")))
         {
             gameObject.GetComponent<Animator>().SetBool("Move", false);
-            gameObject.GetComponent<Animator>().SetBool("Move", false);
-
         }
 
         if (Input.GetKey("d") && Input.GetKey("c"))
         {
-            gameObject.transform.Translate(10f * Time.deltaTime, 0, 0);
+            gameObject.transform.Translate(run_Speed * Time.deltaTime, 0, 0);
         }
 
         if (Input.GetKey("a") && Input.GetKey("c"))
         {
-            gameObject.transform.Translate(-10f * Time.deltaTime, 0, 0);
+            gameObject.transform.Translate(-run_Speed * Time.deltaTime, 0, 0);
         }
-
-        onGround = Physics2D.OverlapCircle(footRef.position, 0.5f, 1 << 8);
 
         if (Input.GetKeyDown("w") && onGround && !Input.GetKey("s"))
         {
-            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce));
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jump_Force));
 
         }
 
-        if (!onGround)
+        if (!onGround && !Input.GetKey("s"))
         {
             gameObject.GetComponent<Animator>().SetBool("Jump", true);
         }
