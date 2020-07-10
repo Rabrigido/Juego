a25efbd39acea27b137c9d6910c275aebbfe5ea2
Javier Rojas <javier.rojas04@alumnos.ucn.cl>
2020-07-09 23:34:38 -0400

    Comienzo Nivel 4.1

---
+++ Assets/Mapa4/CamaraPro.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class CamaraPro : MonoBehaviour
+{
+    public GameObject personaje;
+    public Vector2 minCamPos, maxCampPos;
+    public float smoothTime;
+
+    private Vector2 velocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float posX = Mathf.SmoothDamp(transform.position.x,personaje.transform.position.x, ref velocity.x, smoothTime);
+        float posY = Mathf.SmoothDamp(transform.position.y, personaje.transform.position.y, ref velocity.y, smoothTime);
+        
+        transform.position = new Vector3(Mathf.Clamp(posX, minCamPos.x, maxCampPos.x), Mathf.Clamp(posY, minCamPos.y, maxCampPos.y),transform.position.z);
+       
+    }
+}
---
+++ Assets/Mapa4/Texturas/PlataformaMovil.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+
+public class PlataformaMovil : MonoBehaviour
+{
+    public Transform target;
+    
+    public float speed;
+
+    private Vector3 start;
+    private Vector3 end;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (target != null)
+        {
+            start = transform.position;
+            end = target.position;
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if( target != null) {
+            if(transform.position == end)
+            {
+                Vector3 aux = start;
+                start = end;
+                end = aux;
+            }
+
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if(target != null)
+        {
+
+            float fixedSpeed = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, end, fixedSpeed);
+        }
+    }
+}
