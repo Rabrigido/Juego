9550b761f059f4d72de21fca44f573a6b7c8fb30
Javier Rojas <javier.rojas04@alumnos.ucn.cl>
2020-07-12 21:17:06 -0400

    Se agregó el codigo CheckPlatform
    CheckPlatform sirve para que cuando el player esta sobre una plataforma movil este siga la plataforma

---
+++ Assets/Mapa4/Scripts/DrawLine.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class DrawLine : MonoBehaviour
+{
+    public Transform from;
+    public Transform to;
+
+   void OnDrawGizmos()
+    {
+        if(from != null && to != null)
+        {
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawLine(from.position, to.position);
+            Gizmos.DrawSphere(from.position, 0.15f);
+            Gizmos.DrawSphere(to.position, 0.15f);
+        }
+    }
+}
