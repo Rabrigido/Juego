7351929bb9c1fbc5403c649b1fa2e4da0487b577
Javier Rojas <javier.rojas04@alumnos.ucn.cl>
2020-07-12 23:15:39 -0400

    Se agregó el disparo y un poco del nivel 4.1
    Se agregó y mejoró el disparo para que estubiera sincronizado con el sprite

---
+++ Assets/Mapa4/Scripts/BulletMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMovement : MonoBehaviour
+{
+    
+    public float bulletSpeed;
+    public float bulletLife;
+    public string tag;
+    private GameObject gun;
+    private GameObject player;
+
+    private Rigidbody2D bulletRB;
+    private float originalScaleX;
+    // Start is called before the first frame update
+
+    void Awake()
+    {
+        bulletRB = GetComponent<Rigidbody2D>();
+        gun = GameObject.FindGameObjectWithTag(tag);
+        player = GameObject.FindGameObjectWithTag(tag);
+        originalScaleX = transform.localScale.x;
+
+    }
+
+    void Start()
+    {
+        if (player.GetComponent<SpriteRenderer>().flipX)
+        {
+            transform.localScale = new Vector3(-originalScaleX, transform.localScale.y, transform.localScale.z);
+            bulletRB.velocity = new Vector2(-bulletSpeed, bulletRB.velocity.y);
+        }
+        else if(!player.GetComponent<SpriteRenderer>().flipX)
+        {
+            transform.localScale = new Vector3(originalScaleX, transform.localScale.y, transform.localScale.z);
+            bulletRB.velocity = new Vector2(bulletSpeed, bulletRB.velocity.y);
+
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        Destroy(gameObject, bulletLife);
+    }
+}
