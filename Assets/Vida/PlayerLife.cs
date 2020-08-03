f39cdd551557422e75e54db7c4108ec837f1c788
Javier Rojas <javier.rojas04@alumnos.ucn.cl>
2020-08-03 01:16:12 -0400

    Muchos cambios
    Enemigo
    colliders
    etc.

---
+++ Assets/Vida/PlayerLife.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLife : MonoBehaviour
+{
+    public int vida = 4;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (vida == 0)
+        {
+            gameObject.GetComponent<Animator>().SetBool("Muerte", true);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            if(vida > 0)
+            {
+                vida--;
+            }
+            
+        }
+
+    }
+    public int getVida()
+    {
+        return vida;
+    }
+}
