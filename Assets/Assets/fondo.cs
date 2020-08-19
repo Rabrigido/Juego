14e24a5852756f5d5ce8b23f419acedd6c77371e
nicopina <64911016+nicopina@users.noreply.github.com>
2020-08-19 19:03:39 -0400

    lo mismo pero despues de cerrar unity

--- Assets/Assets/fondo.cs
+++ Assets/Assets/fondo.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class fondo : MonoBehaviour
 {
+    public GameObject camara;
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameObject.transform.position.y() = camara.transform.position.y;
     }
 
     // Update is called once per frame
