9b9e5959581278d75456f47dba252e9c8f5e6782
nicopina <64911016+nicopina@users.noreply.github.com>
2020-08-27 00:28:56 -0400

    avance actualisacion nivel 1

--- Assets/Scenes/Cargas/CargaMenuTutorial1.cs
+++ Assets/Scenes/Cargas/CargaMenuTutorial1.cs
@@ -17,7 +17,7 @@ public class CargaMenuTutorial1 : MonoBehaviour
     void Update()
     {
         contador = contador + Time.deltaTime;
-        if (Input.anyKeyDown) SceneManager.LoadScene("Nivel2.1");
-        if (contador>30) SceneManager.LoadScene("Nivel2.1");
+        if (Input.anyKeyDown) SceneManager.LoadScene("Nivel1.1");
+        if (contador>30) SceneManager.LoadScene("Nivel1.1");
     }
 }
