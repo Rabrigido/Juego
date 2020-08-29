44769a4f0bcb66c3450928b4eecb97a1199a6cc8
Cristhian <cristhian.rabi@alumnos.ucn.cl>
2020-08-29 00:55:01 -0400

    Boss 1

--- Assets/Boss1/Boss1.cs
+++ Assets/Boss1/Boss1.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Boss1 : MonoBehaviour
 {
@@ -10,24 +12,79 @@ public class Boss1 : MonoBehaviour
     public int initialSpeed;
     private float speed2;
     Vector3 target;
+    public float minX;
+    public float maxX;
+    public float maxY;
+    public GameObject puntoInicial;
+    public int vida;
+    public GameObject textoContadorEnemigos;
+    private int contador;
+    public GameObject recolectable;
+    float contadorRecolectable = 0;
+    public GameObject audioMuerteEnemigo;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameObject.GetComponent<Animator>().SetBool("Acercarse", false);
+        PlayerPrefs.SetInt("vidajefe",vida);
+        if (recolectable != null)
+        {
+            recolectable.SetActive(false);
+
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (PlayerPrefs.GetInt("vidajefe") <= 0) //Se muere
+        {
+            gameObject.GetComponent<Animator>().SetBool("Muerto", true);
+            gameObject.GetComponent<Animator>().SetBool("Acercarse", false);
+            gameObject.GetComponent<Animator>().SetBool("Atacar", false);
+            speed2 = 0;
+            contador = Int32.Parse(textoContadorEnemigos.GetComponent<Text>().text);
+            contador--;
+            textoContadorEnemigos.GetComponent<Text>().text = contador.ToString();
+
+            contadorRecolectable = contadorRecolectable + Time.deltaTime;
+
+            if (recolectable != null)
+            {
+
+                if (contadorRecolectable > 1.4)
+                {
+                    if (!recolectable.activeSelf)
+                    {
+                        recolectable.transform.position = gameObject.transform.position;
+                        recolectable.SetActive(true);
+                    }
+
+                }
+
+            }
+        }
+
+        Debug.Log("VIDA BOSS: " + PlayerPrefs.GetInt("vidajefe"));
+
+
         float dist = Vector3.Distance(player.transform.position, transform.position);
-        target = new Vector3(player.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
+        
 
         if (!gameObject.GetComponent<Animator>().GetBool("Muerto")) //Si no está muerto
         {
-            if (dist < visionRadius1) //Lo ve
+            if ((player.transform.position.x >= minX && player.transform.position.x <= maxX) && (player.transform.position.y < maxY)) //Lo ve
             {
-                
+                if (gameObject.transform.position.x - player.transform.position.x > 0) //flipeo
+                {
+                    gameObject.GetComponent<SpriteRenderer>().flipX = true;
+                }
+                if (gameObject.transform.position.x - player.transform.position.x < 0) //flipeo
+                {
+                    gameObject.GetComponent<SpriteRenderer>().flipX = false;
+                }
+
                 gameObject.GetComponent<Animator>().SetBool("Acercarse", true);
 
                 if (gameObject.transform.position.x == player.transform.position.x && 
@@ -39,37 +96,80 @@ public class Boss1 : MonoBehaviour
 
                 if (dist <= visionRadius2) //Está en la distancia para atacar
                 {
-                    gameObject.GetComponent<Animator>().SetBool("Atacar", true);
                     speed2 = 0;
+                    gameObject.GetComponent<Animator>().SetBool("Atacar", true);
+                    gameObject.GetComponent<Animator>().SetBool("Acercarse", false);
                     transform.position = Vector3.MoveTowards(transform.position, target, speed2);
                 }
                 if (dist > visionRadius2)  //No está tan cerca para atacar
                 {
+                    target = new Vector3(player.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
                     gameObject.GetComponent<Animator>().SetBool("Atacar", false);
-                    speed2 = initialSpeed;
-                    speed2 = initialSpeed * Time.deltaTime;
-                    transform.position = Vector3.MoveTowards(transform.position, target, speed2);
+                    gameObject.GetComponent<Animator>().SetBool("Acercarse", true);
+
+                    if (gameObject.transform.position.x > minX && gameObject.transform.position.x < maxX) //Si está dentro de su espacio
+                    {
+       
+                        speed2 = initialSpeed * Time.deltaTime;
+                        transform.position = Vector3.MoveTowards(transform.position, target, speed2); //Te sigue
+                        
+                    }
+
+                    if (gameObject.transform.position.x == minX || gameObject.transform.position.x == maxX) //Llega al borde
+                    {
+                        gameObject.GetComponent<Animator>().SetBool("Acercarse", false);
+                        speed2 = 0;
+                        transform.position = Vector3.MoveTowards(transform.position, target, speed2);
+                    }
+                }  
+            }
+            else
+            {
+
+                gameObject.GetComponent<Animator>().SetBool("Acercarse", false);
+                gameObject.GetComponent<Animator>().SetBool("Atacar", false);
+                speed2 = 0;
+                if (gameObject.transform.position.x - player.transform.position.x > 0) //flipeo
+                {
+                    gameObject.GetComponent<SpriteRenderer>().flipX = true;
+                }
+                if (gameObject.transform.position.x - player.transform.position.x < 0) //flipeo
+                {
+                    gameObject.GetComponent<SpriteRenderer>().flipX = false;
+                }
+            }
+            /*
+            else //No lo ve, por lo tanto se devuelve.
+            {
+
+                if (gameObject.transform.position.x - puntoInicial.transform.position.x > 0) //flipeo
+                {
+                    gameObject.GetComponent<SpriteRenderer>().flipX = true;
+                }
+                if (gameObject.transform.position.x - puntoInicial.transform.position.x < 0) //flipeo
+                {
+                    gameObject.GetComponent<SpriteRenderer>().flipX = false;
                 }
 
-                
+                if (gameObject.transform.position.x == puntoInicial.transform.position.x) //Está en el origen
+                {
+                    gameObject.GetComponent<Animator>().SetBool("Acercarse", false);
+                    speed2 = 0;
+                    transform.position = Vector3.MoveTowards(transform.position, target, speed2);
+                }
+                else //Si no está en el origen
+                {
+                    target = new Vector3(puntoInicial.transform.position.x, transform.position.y, transform.position.z);
+                    Debug.Log("Aaa");
+                    speed2 = initialSpeed * Time.deltaTime;
+                    transform.position = Vector3.MoveTowards(transform.position, target, speed2);
+                    gameObject.GetComponent<Animator>().SetBool("Acercarse", true);
+                }
             }
-            else //No lo ve, por lo tanto no se acerca
-            {
-                gameObject.GetComponent<Animator>().SetBool("Acercarse", false);
-            }
-
-            if (gameObject.transform.position.x - player.transform.position.x > 0)
-            {
-                gameObject.GetComponent<SpriteRenderer>().flipX = true;
-            }
-            if (gameObject.transform.position.x - player.transform.position.x < 0)
-            {
-                gameObject.GetComponent<SpriteRenderer>().flipX = false;
-            }
+            */
+            
         }
     }
-
-
 
     void OnDrawGizmos()
     {
@@ -77,5 +177,14 @@ public class Boss1 : MonoBehaviour
         Gizmos.DrawWireSphere(transform.position, visionRadius1);
         Gizmos.color = UnityEngine.Color.red;
         Gizmos.DrawWireSphere(transform.position, visionRadius2);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+
+    {
+        if (collision.gameObject.tag == "Bala")
+        {
+            PlayerPrefs.SetInt("vidajefe", (PlayerPrefs.GetInt("vidajefe") - 1));
+        }
     }
 }
