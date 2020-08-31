78fd2f0716ee8096a723a2a2353744bacf6c5cad
nicopina <64911016+nicopina@users.noreply.github.com>
2020-08-31 19:01:59 -0400

    cargas

---
+++ Assets/Scenes/Cargas/N1/carga112.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class carga112 : MonoBehaviour
+{
+    private float contador = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        contador = contador + Time.deltaTime;
+        if (contador > 3) SceneManager.LoadScene("Nivel1.2");
+    }
+}
---
+++ Assets/Scenes/Cargas/N4/carga412.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class carga412 : MonoBehaviour
+{
+    private float contador = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        contador = contador + Time.deltaTime;
+        if (contador > 3) SceneManager.LoadScene("Nivel4.2");
+    }
+}
