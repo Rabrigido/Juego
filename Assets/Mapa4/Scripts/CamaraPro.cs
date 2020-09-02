3a2192fb35fd8cac0f41e0480e628aff617e0ebf
nicopina <64911016+nicopina@users.noreply.github.com>
2020-09-02 00:17:53 -0400

    arreglos camara build 0.1

--- Assets/Mapa4/Scripts/CamaraPro.cs
+++ Assets/Mapa4/Scripts/CamaraPro.cs
@@ -23,7 +23,7 @@ public class CamaraPro : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (Input.GetKey("s"))
+        if (Input.GetKey("s") && player.GetComponent<Player1controller>().onGround)
         {
             contAux = contAux + Time.deltaTime;
             if (contAux >= 0.4f)
@@ -39,6 +39,23 @@ public class CamaraPro : MonoBehaviour
                 {
                     float posX = Mathf.SmoothDamp(transform.position.x, player.transform.position.x + 10f, ref velocity.x, smoothTime);
                     float posY = Mathf.SmoothDamp(transform.position.y, player.transform.position.y + 0f, ref velocity.y, smoothTime - .2f);
+
+                    transform.position = new Vector3(Mathf.Clamp(posX, minCamPos.x, maxCampPos.x), Mathf.Clamp(posY, minCamPos.y, maxCampPos.y), transform.position.z);
+                }
+            }
+            else
+            {
+                if (player.GetComponent<SpriteRenderer>().flipX)
+                {
+                    float posX = Mathf.SmoothDamp(transform.position.x, player.transform.position.x - 10f, ref velocity.x, smoothTime);
+                    float posY = Mathf.SmoothDamp(transform.position.y, player.transform.position.y + 8f, ref velocity.y, smoothTime - .2f);
+
+                    transform.position = new Vector3(Mathf.Clamp(posX, minCamPos.x, maxCampPos.x), Mathf.Clamp(posY, minCamPos.y, maxCampPos.y), transform.position.z);
+                }
+                else
+                {
+                    float posX = Mathf.SmoothDamp(transform.position.x, player.transform.position.x + 10f, ref velocity.x, smoothTime);
+                    float posY = Mathf.SmoothDamp(transform.position.y, player.transform.position.y + 8f, ref velocity.y, smoothTime - .2f);
 
                     transform.position = new Vector3(Mathf.Clamp(posX, minCamPos.x, maxCampPos.x), Mathf.Clamp(posY, minCamPos.y, maxCampPos.y), transform.position.z);
                 }
